3ec30e39d90d687688fefbe4315c4fd766547dd9
Lukadevel <luka84saric@gmail.com>
2023-12-05 20:27:08 +0100

    logo-InputComponent

---
+++ src/components/InputComponent.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function InputComponent() {
+  return (
+    <div>
+      <input type="text" placeholder="Search..." />
+      <button>Search</button>
+    </div>
+  );
+}
+
+export default InputComponent;
--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import logo from "../assets/logo 1.png";
+import logo from "../assets/logo.png";
+import InputComponent from "./InputComponent";
 
 function NavbarComponent() {
   return (
-    <div className=" bg-mainBlue h-[100px]">
-      <div className="container mx-auto">
+    <div className=" bg-mainBlue h-[100px] ">
+      <div className="container w-[90%] mx-auto">
         <Link to="/">
           <img src={logo} alt="e-shop" />
         </Link>
+        <InputComponent />
       </div>
     </div>
   );
