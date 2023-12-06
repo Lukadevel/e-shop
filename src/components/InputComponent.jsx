15d1b7dd8987b53287bfb035b4120e8da9cccc04
Lukadevel <luka84saric@gmail.com>
2023-12-06 17:40:17 +0100

    icons, input and navbar

--- src/components/InputComponent.jsx
+++ src/components/InputComponent.jsx
@@ -2,9 +2,15 @@ import React from "react";
 
 function InputComponent() {
   return (
-    <div>
-      <input type="text" placeholder="Search..." />
-      <button>Search</button>
+    <div className="bg-[#fff] rounded-[20px] overflow-hidden flex">
+      <input
+        className=" pl-[25px] py-[17px]"
+        type="text"
+        placeholder="Search..."
+      />
+      <button className="bg-mainYellow text-[#fff] px-[30px] rounded-[20px]">
+        Search
+      </button>
     </div>
   );
 }
--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -1,17 +1,35 @@
-import React from "react";
 import { Link } from "react-router-dom";
-
-import logo from "../assets/logo.png";
 import InputComponent from "./InputComponent";
+import logo from "../assets/logo.png";
+
+//icons
+
+import { FaUser, FaHeart, FaShoppingCart } from "react-icons/fa";
 
 function NavbarComponent() {
   return (
     <div className=" bg-mainBlue h-[100px] ">
-      <div className="container w-[90%] mx-auto">
+      <div className="container w-[90%] h-[100px] mx-auto flex justify-between items-center ">
         <Link to="/">
           <img src={logo} alt="e-shop" />
         </Link>
+
         <InputComponent />
+
+        <div className="flex">
+          <div>
+            <FaUser className="text-[#fff]" />
+            <span className="text-[#fff]">Sing in</span>
+          </div>
+          <div>
+            <FaHeart className="text-[#fff]" />
+            <span className="bg-mainYellow">0</span>
+          </div>
+          <div>
+            <FaShoppingCart className="text-[#fff]" />
+            <span className="bg-mainYellow">0</span>
+          </div>
+        </div>
       </div>
     </div>
   );
